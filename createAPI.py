19ad7351dd8629db9a5350f39468bb2acb5a97f1
SDW1014 <apif730@gmail.com>
2024-02-23 18:31:32 +0900

    [refactor] createAPI 주석 추가 - 기본적인 주석들을 함수에 추가하였다.

--- createAPI.py
+++ createAPI.py
@@ -1,5 +1,6 @@
 import os
 
+# 기본이 되는 함수
 def create_folder(folder_name):
     try:
         os.mkdir(folder_name)
@@ -8,11 +9,13 @@ def create_folder(folder_name):
         print(f"'{folder_name}' 폴더가 이미 존재합니다.")
     except Exception as e:
         print(f"폴더 생성 중 오류가 발생했습니다: {e}")
-        
+
+# 배열을 이용해 create_folder(folder_name)를 사용한다.
 def create_folder_array(folder_names):
     for name in folder_names:
         create_folder(name)
-        
+
+# 딕셔너리(맵)을 이용해 create_folder(folder_name)를 사용한다.
 def create_folder_map(folder_names_map_version):
     for key, value in folder_names_map_version.items():
         create_folder(value)
